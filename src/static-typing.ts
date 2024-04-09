22d79134f22e6020a4e625758459b39f4ff53cbf
Igor Dossin Lazzaretti <idlazzaretti@gmail.com>
2024-04-09 15:56:24 -0300

    commit

---
+++ src/static-typing.ts
@@ -0,0 +1,13 @@
+/**
+ * Tipos primitivos
+ * Number> Numeros Decimais
+ * String > Textos, Caracteres
+ * Booleano > Verdadeiro ou falso
+ */
+
+// Tipagem Estática no typescript:
+let age:number = '22';
+const fullName:string = 'Agustinho';
+let isAlive: boolean = true;
+
+
